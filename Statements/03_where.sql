452d58001f9849145d8df1b4c467ca562aeb9d38
Angel Antonio Gomera <antoniogomera12@gmail.com>
2026-04-05 16:09:05 -0400

    add WHERE clause examples for user age filtering

--- Statements/03_where.sql
+++ Statements/03_where.sql
@@ -1,1 +1,3 @@
 SELECT * FROM users WHERE age = 21; -- Este comando selecciona todas las columnas de la tabla "users" pero solo muestra los registros donde la edad es igual a 21.
+
+SELECT name FROM users WHERE age = 22; -- Este comando selecciona solo la columna "name" de la tabla "users" pero solo muestra los nombres de los usuarios donde la edad es igual a 22.
