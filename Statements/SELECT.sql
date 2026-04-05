8c52ea41cf7aeaf3ce33245c966733654bb5d98a
Angel Antonio Gomera <antoniogomera12@gmail.com>
2026-04-05 15:05:37 -0400

    add SELECT statements for retrieving user data

---
+++ Statements/SELECT.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users; -- Este comando selecciona todas las columnas de la tabla "users" y muestra todos los registros.
+
+SELECT name FROM users; -- Este comando selecciona solo la columna "name" de la tabla "users" y muestra los nombres de todos los usuarios en esa columna.
+
+SELECT name, surname FROM users; -- Este comando selecciona las columnas "name" y "surname" de la tabla "users" y muestra los nombres y apellidos de todos los usuarios.
