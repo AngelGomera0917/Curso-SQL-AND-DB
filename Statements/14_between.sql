8f33b430585516ca8c7f0745136147f6b709f606
Angel Antonio Gomera <antoniogomera12@gmail.com>
2026-04-12 11:05:44 -0400

    add SQL statements for selecting users within and outside a specific age range

---
+++ Statements/14_between.sql
@@ -0,0 +1,4 @@
+SELECT * FROM users WHERE age BETWEEN 22 AND 30; -- Este comando selecciona todos los registros de la tabla "users" donde el valor de la columna "age" esté entre 22 y 30, incluyendo ambos valores. El operador BETWEEN se utiliza para filtrar los resultados dentro de un rango específico, en este caso, se mostrarán todos los usuarios cuya edad esté entre 22 y 30 años.
+
+SELECT * FROM users WHERE age NOT BETWEEN 22 AND 30; -- Este comando selecciona todos los registros de la tabla "users" donde el valor de la columna "age" no esté entre 22 y 30, excluyendo ambos valores. El operador NOT BETWEEN se utiliza para filtrar los resultados fuera de un rango específico, en este caso, se mostrarán todos los usuarios cuya edad no esté entre 22 y 30 años.
+
