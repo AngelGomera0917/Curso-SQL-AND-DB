593817f292295dd4c62606d7096cd3d8dff03084
Angel Antonio Gomera <antoniogomera12@gmail.com>
2026-04-12 16:06:52 -0400

    add SQL statements to insert a user with null email and update surname for user ID 2

---
+++ Database/02_update.sql
@@ -0,0 +1,2 @@
+UPDATE users SET surname = 'Gomera Diaz' WHERE user_id = 2; -- Este comando actualiza el apellido del usuario con el ID 2 en la tabla "users". El nuevo apellido se establece como "Gomera Diaz". La cláusula WHERE se utiliza para especificar que solo se debe actualizar el registro del usuario con el ID 2, asegurando que no se modifiquen otros registros en la tabla.
+
